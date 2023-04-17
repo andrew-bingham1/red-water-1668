88b73170853e30f69f24a5b746da667db5ba1d69
Andrew Bingham <andrewgbingham@gmail.com>
2023-04-17 14:37:54 -0600

    feat: update show page with form to add new ingredients

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+@chef_1 = Chef.create!(name: "Gordon Ramsay")
+@dish_1 = @chef_1.dishes.create!(name: "Mac and Cheese", description: "Macaroni noodles with cheese")
+@ingredient_1 = Ingredient.create!(name: "Macaroni", calories: 200)
+@ingredient_2 = Ingredient.create!(name: "Cheese", calories: 400)
+@ingredient_3 = Ingredient.create!(name: "Bacon", calories: 100)
+@dish_ingredients_1 = DishIngredient.create!(dish: @dish_1, ingredient: @ingredient_1)
+@dish_ingredients_2 = DishIngredient.create!(dish: @dish_1, ingredient: @ingredient_2)
